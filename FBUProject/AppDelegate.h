2a2ac9b30d8be4fd68478245ff2ebace1b14aed8
Kalkidan Tamirat <kalkidant@fb.com>
2021-07-12 08:59:46 -0700

    Initial Commit

---
+++ FBUProject/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  FBUProject
+//
+//  Created by Kalkidan Tamirat on 7/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ FBUProject/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  FBUProject
+//
+//  Created by Kalkidan Tamirat on 7/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
